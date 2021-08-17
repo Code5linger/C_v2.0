5b6115e8d673087312b49ddaa13be56af233cb72
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-17 16:19:25 +0600

    Initial commit

---
+++ JasonFedin-MasterThe_C_Language/cmake-build-debug/CMakeFiles/JasonFedin_MasterThe_C_Language.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JasonFedin_MasterThe_C_Language.dir/main.c.obj"
+  "JasonFedin_MasterThe_C_Language.exe"
+  "JasonFedin_MasterThe_C_Language.exe.manifest"
+  "JasonFedin_MasterThe_C_Language.pdb"
+  "libJasonFedin_MasterThe_C_Language.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JasonFedin_MasterThe_C_Language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ JasonFedin-MasterThe_C_Language/main.c
@@ -0,0 +1,20 @@
+/*
+ * Author: CodeSlinger
+ * Date: 17/08/2021
+ */
+
+#include <stdio.h>
+/*
+int main() {
+    printf("Hello World\n");
+    return 17;
+}
+*/
+int main(){
+    printf("\nHi, My name is CodeSlinger\n");
+    return 87;
+    //Single Line Comment
+    /*
+     * Multi Line Comment
+     */
+}
