288c58bdd03dba318bba9cd209b6d3664f9f38f3
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-18 22:33:53 +0600

    18-08

--- JasonFedin-MasterThe_C_Language/main.c
+++ JasonFedin-MasterThe_C_Language/main.c
@@ -40,10 +40,44 @@ int main(){
     return 0;
 }
  */
+/*
 #include <stdio.h>
 
 int main(){
     printf("\nHi! My name is Codeslinger.\n");
     return  87;
-}
+}*/
+/*
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i;
 
+    printf("Enter a value:");
+    scanf("%d %s", &i, str);
+
+    printf("\nYou entered: %d:::::%s\n", i, str);
+
+    return 0;
+}
+ */
+/*
+ * %s       = String            geeksforgeeks
+ * %d %i    = Signed Integer    45 | 45
+ * %u       = Unsigned Integer 4294967286 | 10
+ * %c       = Character         A-Z
+ * %f | %e | %E = Floating-point    1.267000e+01
+ *
+ */
+#include <stdio.h>
+
+int main(){
+    char str[100];
+    int i;
+
+    printf("Enter a number:");
+    scanf("%i", &i);
+
+    printf("\nYour number is: %i \n", i);
+    return 0;
+}
