50fc8374424cb0770a932553fcbaaff1ec59296a
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2021-08-18 19:22:39 +0600

    18-08

--- JasonFedin-MasterThe_C_Language/main.c
+++ JasonFedin-MasterThe_C_Language/main.c
@@ -2,9 +2,9 @@
  * Author: CodeSlinger
  * Date: 17/08/2021
  */
-
-#include <stdio.h>
 /*
+#include <stdio.h>
+
 int main() {
     printf("Hello World\n");
     return 17;
@@ -15,8 +15,8 @@ int main(){
     printf("\nHi, My name is CodeSlinger\n");
     return 87;
     //Single Line Comment
-    /*
-     * Multi Line Comment
+
+     Multi Line Comment
 
 }
 */
@@ -26,7 +26,24 @@ int main(){
     return 17;
 }
 */
+/*
 int main() {
     printf("Hello World");
     return  87;
 }
+ */
+/*
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+    return 0;
+}
+ */
+#include <stdio.h>
+
+int main(){
+    printf("\nHi! My name is Codeslinger.\n");
+    return  87;
+}
+
